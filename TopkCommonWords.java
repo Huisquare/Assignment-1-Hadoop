41343d603984f1884191904939644aaf90275fdb
Huihui <lihuihui99@outlook.com>
2021-10-01 16:01:09 +0800

    included CompositeKey class

--- TopkCommonWords.java
+++ TopkCommonWords.java
@@ -1,5 +1,6 @@
 // Matric Number: A0188608N
 // Name: Li Huihui
+import java.io.DataInput;
 import java.io.IOException;
 import java.util.StringTokenizer;
 
@@ -7,6 +8,7 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
@@ -15,6 +17,7 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class TopkCommonWords {
 
+	// First Mapper - will output (word-text-i , 1)
     public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
 
         private final static IntWritable one = new IntWritable(1);
@@ -58,4 +61,57 @@ public class TopkCommonWords {
         FileOutputFormat.setOutputPath(job, new Path(args[3]));
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
+}
+
+public class CompositeKey implements WritableComparable<CompositeKey> {
+	private String word;
+	private int source; // source is either 1 or 2 (input 1 or input 2)
+
+	public String getWord() {
+		return this.word;
+	}
+
+	public int getSource() {
+		return this.source;
+	}
+
+	@Override
+	public void write(DataOutput out) throws IOException {
+		out.writeString(word);
+		out.writeInt(source);
+	}
+
+	@Override
+	public void readFields(DataInput in) throws IOException {
+		word = in.readLine();
+		source = in.readInt();
+	}
+
+	@Override
+	public int compareTo(CompositeKey other) {
+		return ComparisonChain.start().compare(word, other.word).compare(source, other.source).result();
+	}
+
+	@Override
+	public boolean equals(CompositeKey other) {
+		if (this.word.equals(other.getWord()) && (this.source == other.getSource())) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "(" + word + ", " + source + ")";
+	}
+
+	@Override
+	public int hashCode() { // example taken from hadoop WritableComparable API
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + word.hashCode();
+		result = result + (int) (source ^ source >>> 32);
+		return result;
+	}
 }
